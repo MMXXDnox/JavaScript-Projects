d1386a14973c84518e9b88bc7b711670d8fed4ef
MMXXDnox <mmxxdnox@proton.me>
2026-01-03 01:50:24 +0000

    Project2_Functions Assignment
    Project2_Functions Assignment
    
     function that uses the += operator

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,1 +1,12 @@
 //Project2 Functions Assignment
+
+//a function that uses the += operator
+function myFunction() {
+    var sentence = "I'm learning"; //creating the variable
+    sentence += " a lot from this course!"; //using the += operator to add to the  variable
+    document.getElementById("Concatenate").innerHTML = sentence; //using document.getElementById()
+}
+//Display date button function
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
